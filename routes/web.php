fb7cf517ecf10e061b8ede3fa9fe5bec00daf6e9
rynhsn <riyan.husen@gmail.com>
2021-01-17 02:53:56 +0700

    Create, Read, Update

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => 'checkSession'], function () {
     Route::get('/',  'HomeController@index');
+    Route::get('/user', 'UserController@index');
+    Route::get('/list', 'UserController@list');
+    Route::get('/add', 'UserController@tambah');
+    Route::get('/edit/{id}', 'UserController@edit');
+
+    Route::post('/simpan', 'UserController@simpan');
+    Route::post('/update', 'UserController@update');
 });
+
 Route::get('/login', 'HomeController@login');
 Route::post('/masuk', 'HomeController@masuk');
